b97290a9435a5f4eca895e3069b8340a76900be1
ArabGangDev <41093102+ArabGangDev@users.noreply.github.com>
2018-12-20 17:54:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -179,4 +179,44 @@ member.send( `${member} ` + "**" + `MegaMcBroadcast v0.3.8` + " : ** " + message
 															
 });
 });
+
+
+client.on('message', message => {
+    if (message.content.startsWith(".bot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Broadcast Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[-]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                    })
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
